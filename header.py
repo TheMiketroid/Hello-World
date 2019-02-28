fc54f90b661f44ecd81191bc35757d3623cc69c6
TheMiketroid <48102672+TheMiketroid@users.noreply.github.com>
2019-02-28 14:00:49 -0500

    Add files via upload

---
+++ header.py
@@ -0,0 +1,14 @@
+#basic header function
+
+import datetime
+dateAndTime = datetime.date.today()
+
+def test_header():
+    print("Program by Mike Gewlas.")
+    print(dateAndTime)
+    print("Hello World Program")
+    print("mikegewlas@gmail.com")
+
+test_header()
+
+print("Hello, World!")
